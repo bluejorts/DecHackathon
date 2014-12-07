036c1d97c9b3be66288f3cab08c9e27d6d0b1131
Austin Paxton <Polymetric@users.noreply.github.com>
2014-12-07 15:53:18 -0600

    Delete tts_pocketsphinx.rb

--- tts_pocketsphinx.rb
+++
@@ -1,7 +0,0 @@
-require 'pocketsphinx-ruby'
-
-configuration = Pocketsphinx::Configuration::KeywordSpotting.new('Okay computer')
-recognizer = Pocketsphinx::LiveSpeechRecognizer.new(configuration)
-recognizer.recognize do |speech|
-	puts speech
-end
